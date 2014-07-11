8a01cfc31d075467b3951f8454fa4099f0f61b2f
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2014-07-11 13:32:42 +0900

    modify template/Kruskal.cpp

--- template/Kruskal.cpp
+++ template/Kruskal.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+
 // O(E*log(V))
+
+struct edge { int u, v, cost; };
+
+bool comp(const edge& e1, const edge& e2) {
+    return e1.cost < e2.cost;
+}
+
+edge es[MAX_E];
+int V, E;
+
+int kruskal() {
+    sort(es, es + E, comp);
+    init_union_find(V);
+    for res = 0;
+    for(int i = 0; i < E; i++) {
+        edge e = es[i];
+        if(!same(e.u, e.v)) {
+            unite(e.u, e.v);
+            res += e.cost;
+        }
+    }
+    return res;
+}
