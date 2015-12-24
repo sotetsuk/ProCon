77fc95a9cf227a8b496926075ef65e84d54996cc
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2015-12-24 09:10:13 +0900

    solve ABC#1, #2 A problem

---
+++ ABC/002/A.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by sotetsuk on 2015/12/24.
+ */
+object A2 {
+  def main(args:Array[String]):Unit = {
+    val x = readLine.split(" ").map(_.toInt)
+    if (x(0) > x(1)) {
+      println(x(0))
+    } else {
+      println(x(1))
+    }
+  }
+}
