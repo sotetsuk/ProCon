e5c404ea1058e6345f430e844590c1e4c0fd6bbf
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-06-11 23:17:42 +0900

    attend ABC039

---
+++ ABC/039/A.py
@@ -0,0 +1,2 @@
+A, B, C = [int(x) for x in input().split()]
+print(2 * ((A * B) + (B * C) + (C * A)))
---
+++ ABC/039/C.py
@@ -0,0 +1,24 @@
+s = input()
+
+Do = "WBWBWWBWBWBWWBWBWWBW"
+Re = "WBWWBWBWBWWBWBWWBW"
+Mi = "WWBWBWBWWBWBWWBW"
+Fa = "WBWBWBWWBWBWWBW"
+So = "WBWBWWBWBWWBW"
+La = "WBWWBWBWWBW"
+Si = "WWBWBWWBW"
+
+if s.startswith(Do):
+    print("Do")
+elif s.startswith(Re):
+    print("Re")
+elif s.startswith(Mi):
+    print("Mi")
+elif s.startswith(Fa):
+    print("Fa")
+elif s.startswith(So):
+    print("So")
+elif s.startswith(La):
+    print("La")
+elif s.startswith(Si):
+    print("Si")
