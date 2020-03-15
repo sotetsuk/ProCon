2b9f131302c24f01794a22fa49215cbfe11c470f
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-03-16 08:13:11 +0900

    update template

--- template/cpp/main.cpp
+++ template/cpp/main.cpp
@@ -9,10 +9,23 @@
 #include <stack>
 #include <queue>
 #include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>>
 #include <sstream>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+// namespace
 using namespace std;
-typedef pair<int, int> pii;
-typedef long long int ll;
+namespace mp = boost::multiprecision;
+
+// types
+using pii = pair<int, int>;
+using ll = long long int;
+using ld = long double;
+using mpi = mp::cpp_int;
+using mpf = mp::number<mp::cpp_dec_float<1024>>;
 
 int main() {
     return 0;
