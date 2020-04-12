63975781e6cc6ffdbef05bea8885887838ec9f0a
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-04-12 20:50:35 +0900

    tmp

---
+++ Selected100/008.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <sstream>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+// namespace
+using namespace std;
+namespace mp = boost::multiprecision;
+
+// types
+using pii = pair<int, int>;
+using ll = long long int;
+using ld = long double;
+using mpi = mp::cpp_int;
+using mpf = mp::number<mp::cpp_dec_float<1024>>;
+
+int main() {
+    int n, t;
+    double am = 0, bm = 0;
+    vector<int> a;
+    vector<int> b;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> t;
+        a.push_back(t);
+        am += t;
+        cin >> t;
+        b.push_back(t);
+        bm += t;
+    }
+    am /= n;
+    bm /= n;
+    return 0;
+}
