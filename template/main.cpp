c55e82b5a4ee3a5aa9be4067419c5ece7c68bf6b
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-04-19 23:44:49 +0900

    update template

--- template/main.cpp
+++ template/main.cpp
@@ -14,22 +14,44 @@
 #include <unordered_set>
 #include <sstream>
 #include <numeric>
-#include <boost/multiprecision/cpp_dec_float.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
+// #include <boost/multiprecision/cpp_dec_float.hpp>
+// #include <boost/multiprecision/cpp_int.hpp>
 
+///////////////////////////////////////////////////////////////
 // namespace
+///////////////////////////////////////////////////////////////
 using namespace std;
-namespace mp = boost::multiprecision;
+// namespace mp = boost::multiprecision;
 
+///////////////////////////////////////////////////////////////
 // types
+///////////////////////////////////////////////////////////////
 using pii = pair<int, int>;
 using ll = long long int;
 using ld = long double;
-using mpi = mp::cpp_int;
-using mpf = mp::number<mp::cpp_dec_float<1024>>;
+// using mpi = mp::cpp_int;
+// using mpf = mp::number<mp::cpp_dec_float<1024>>;
 
-// consts
-int MOD = 1000000007;  // https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+///////////////////////////////////////////////////////////////
+// mod https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+///////////////////////////////////////////////////////////////
+// ll MOD = 1000000007;
+// ll mod(long long val) {
+//     long long res = val % MOD;
+//     if (res < 0) res += MOD;
+//     return res;
+// }
+// ll modinv(ll a) {
+//     ll b = MOD, u = 1, v = 0;
+//     while (b) {
+//         ll t = a / b;
+//         a -= t * b; swap(a, b);
+//         u -= t * v; swap(u, v);
+//     }
+//     u %= MOD;
+//     if (u < 0) u += MOD;
+//     return u;
+// }
 
 
 int main() {
