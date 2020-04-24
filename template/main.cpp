3abb867133d4e9a3b29d792188f2f866826ea506
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-04-25 07:08:40 +0900

    update

--- template/main.cpp
+++ template/main.cpp
@@ -26,9 +26,10 @@ using namespace std;
 ///////////////////////////////////////////////////////////////
 // types
 ///////////////////////////////////////////////////////////////
-using pii = pair<int, int>;
 using ll = long long int;
 using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 // using mpi = mp::cpp_int;
 // using mpf = mp::number<mp::cpp_dec_float<1024>>;
 
@@ -36,11 +37,13 @@ using ld = long double;
 // mod https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
 ///////////////////////////////////////////////////////////////
 // ll MOD = 1000000007;
+//
 // ll mod(long long val) {
 //     long long res = val % MOD;
 //     if (res < 0) res += MOD;
 //     return res;
 // }
+//
 // ll modinv(ll a) {
 //     ll b = MOD, u = 1, v = 0;
 //     while (b) {
@@ -51,6 +54,24 @@ using ld = long double;
 //     u %= MOD;
 //     if (u < 0) u += MOD;
 //     return u;
+// }
+//
+// const int MAX = -1;
+// ll fac[MAX], finv[MAX], inv[MAX];
+// void com_init() {  // initialize nCk table with O(N)
+//     fac[0] = fac[1] = 1;
+//     finv[0] = finv[1] = 1;
+//     inv[1] = 1;
+//     for (int i = 2; i < MAX; i++){
+//         fac[i] = fac[i - 1] * i % MOD;
+//         inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+//         finv[i] = finv[i - 1] * inv[i] % MOD;
+//     }
+// }
+// ll com(int n, int k){  // nCk
+//     if (n < k) return 0;
+//     if (n < 0 || k < 0) return 0;
+//     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 // }
 
 
