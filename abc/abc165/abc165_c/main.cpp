4509ea559d9e83b9e478b408c4b50ff1987063ec
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-05-02 23:44:33 +0900

    attend abc165

---
+++ abc/abc165/abc165_c/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <sstream>
+#include <numeric>
+// #include <boost/multiprecision/cpp_dec_float.hpp>
+// #include <boost/multiprecision/cpp_int.hpp>
+
+///////////////////////////////////////////////////////////////
+// namespace
+///////////////////////////////////////////////////////////////
+using namespace std;
+// namespace mp = boost::multiprecision;
+
+///////////////////////////////////////////////////////////////
+// types
+///////////////////////////////////////////////////////////////
+using ll = long long int;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+// using mpi = mp::cpp_int;
+// using mpf = mp::number<mp::cpp_dec_float<1024>>;
+
+///////////////////////////////////////////////////////////////
+// mod https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+///////////////////////////////////////////////////////////////
+// ll MOD = 1000000007;
+//
+// ll mod(long long val) {
+//     long long res = val % MOD;
+//     if (res < 0) res += MOD;
+//     return res;
+// }
+//
+// ll modinv(ll a) {
+//     ll b = MOD, u = 1, v = 0;
+//     while (b) {
+//         ll t = a / b;
+//         a -= t * b; swap(a, b);
+//         u -= t * v; swap(u, v);
+//     }
+//     u %= MOD;
+//     if (u < 0) u += MOD;
+//     return u;
+// }
+//
+// const int MAX = -1;
+// ll fac[MAX], finv[MAX], inv[MAX];
+// void com_init() {  // initialize nCk table with O(N)
+//     fac[0] = fac[1] = 1;
+//     finv[0] = finv[1] = 1;
+//     inv[1] = 1;
+//     for (int i = 2; i < MAX; i++){
+//         fac[i] = fac[i - 1] * i % MOD;
+//         inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+//         finv[i] = finv[i - 1] * inv[i] % MOD;
+//     }
+// }
+// ll com(int n, int k){  // nCk
+//     if (n < k) return 0;
+//     if (n < 0 || k < 0) return 0;
+//     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+// }
+
+struct P {ll a, b, c, d;};
+
+int calc(const vector<ll > &curr, const vector<P> &v) {
+    ll ret = 0;
+    for (const auto &p: v) {
+        if (curr[p.b] - curr[p.a] == p.c) ret += p.d;
+    }
+    return ret;
+}
+
+ll dfs(ll c, ll m, ll n, vector<ll> &curr, const vector<P> &v) {
+    if (c == n) return calc(curr, v);
+    ll i = (!curr.empty()) ? curr.back(): 1;
+    ll ret = 0;
+    for (ll j = i; j <= m; ++j) {
+        curr.push_back(j);
+        ret = max(ret, dfs(c + 1, m, n, curr, v));
+        curr.pop_back();
+    }
+    return ret;
+}
+
+
+int main() {
+    ll n, m, q; cin >> n >> m >> q;
+    ll a, b, c, d;
+    vector<P> v;
+    for (ll i = 0; i < q; ++i) {
+        cin >> a >> b >> c >> d; --a; --b;
+        v.emplace_back(P{a, b, c, d});
+    }
+    vector<ll> curr;
+    cout << dfs(0, m, n, curr, v) << endl;
+    return 0;
+}
