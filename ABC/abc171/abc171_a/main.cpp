ab7dbce76ff6bfd73162e76775e968679fe394e1
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-06-22 02:40:39 +0900

    update

--- ABC/abc171/abc171_a/main.cpp
+++ ABC/abc171/abc171_a/main.cpp
@@ -1,13 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
-    char a; cin >> a;
-    if ('A' <= a  && a <= 'Z') {
-        cout << "A" << endl;
-    } else {
-        cout << "a" << endl;
-    }
+    char a; std::cin >> a;
+    if (isupper(a)) cout << "A" << endl;
+    else cout << "a" << endl;
     return 0;
 }
