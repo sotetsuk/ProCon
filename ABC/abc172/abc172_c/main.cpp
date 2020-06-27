7af11d40947276aa5e20eff62995f08643076e69
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-06-28 01:36:35 +0900

    update

--- ABC/abc172/abc172_c/main.cpp
+++ ABC/abc172/abc172_c/main.cpp
@@ -4,19 +4,17 @@ using ll = long long;
 
 int main() {
     ll N, M, K; cin >> N >> M >> K;
-    vector<int> A(N), B(M);
+    vector<ll> A(N), B(M), S(M+1, 0);
     for (int i = 0; i < N; ++i) cin >> A[i];
     for (int i = 0; i < M; ++i) cin >> B[i];
-    ll s = accumulate(B.begin(), B.end(), 0LL);
-    int j = M;
-    int ans = 0;
+    for (int i = 0; i < M; ++i) S[i+1] = S[i] + B[i];
+    ll sum = 0;
+    ll ans = 0;
     for (int i = 0; i <= N; ++i) {
-        while (j > 0 && K < s) {
-            --j;
-            s -= B[j];
-        }
-        if (K >= s) ans = max(ans, i + j);
-        if (i < N) s += A[i];
+        if (sum > K) break;
+        ll j = upper_bound(S.begin(), S.end(), K - sum) - S.begin() - 1;
+        ans = max(ans, i + j);
+        if (i < N) sum += A[i];
     }
     cout << ans << endl;
     return 0;
