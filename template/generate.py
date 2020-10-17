e6f2c5fb558d372efd7833825e885db7a4fee4a8
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-18 04:47:30 +0900

    update template

--- template/generate.py
+++ template/generate.py
@@ -4,14 +4,14 @@
 https://online-judge-tools.readthedocs.io/en/master/introduction.ja.html
 
 $ oj g/i ./generate.py
-# oj g/o -c ./tle.out  
+$ oj g/o -c ./tle.out  
 """
 
 import random
-N = random.randint(1, 100)
-W = random.randint(1, 10000)
+N = random.randrange(1, 101)
+W = random.randrange(1, 10001)
 print(N, W)
 for _ in range(N):
-    v = random.randint(1, 1000)
-    w = random.randint(1, 1000)
+    v = random.randrange(1, 1001)
+    w = random.randrange(1, 1001)
     print(v, w)
